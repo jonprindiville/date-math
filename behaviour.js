874c009cdc5fdc551d604571e5700d4e823323f7
Jon Prindiville <jon.prindiville@gmail.com>
2013-06-07 18:13:00 -0400

    First emi-working revision. No increment modifications yet.

--- behaviour.js
+++ behaviour.js
@@ -1,20 +1,114 @@
-// Called on adjustment of any parameters to recalculate result
-function refreshResult() {
-}
+// Parameters
+var pivot    = new Date(); // Today
+var op       = "+";
+var incr     = 3;
+var incrUnit = "weeks";
+var result   = null;
 
-YUI().use('overlay', 'calendar', 'node', function (Y) {
+YUI().use('overlay', 'calendar', 'node', 'event', function (Y) {
+
+    // References to the UI bits
     var datePivot    = Y.one("#date-pivot");
     var dateOp       = Y.one("#date-op");
     var dateIncr     = Y.one("#date-incr");
     var dateIncrUnit = Y.one("#date-incr-unit");
     var dateResult   = Y.one("#date-result");
 
-    datePivot.on('click', function (e) {
-        // display calendar overlay
+    // Do whatever operation the UI currently shows
+    function doOp(a, b) {
+        if (op == "+") return a + b;
+        if (op == "-") return a - b;
+        return null;
+    }
+
+    // Called on adjustment of any parameters to recalculate result
+    function refreshDisplay() {
+
+        // Update display of inputs
+        datePivot.setHTML(Y.DataType.Date.format(pivot, {format:"%F"}));
+        dateOp.setHTML(op);
+        dateIncr.setHTML(incr);
+        dateIncrUnit.setHTML(incrUnit);
+        
+        // Compute new result
+        newResult = new Date(pivot);
+        if (incrUnit == "days") {
+            newResult.setDate(doOp(pivot.getDate(),incr));
+        } else if (incrUnit == "weeks") {
+            newResult.setDate(doOp(pivot.getDate(),incr*7));
+        } else if (incrUnit == "months") {
+            newResult.setMonth(doOp(pivot.getMonth(),incr));
+        }
+
+        // Update result
+        result = newResult;
+        dateResult.setHTML(Y.DataType.Date.format(result, {format:"%e %b %Y"}));
+        dateResult.render();
+    }
+
+    Y.one('html').on('load', function (e) {
+        refreshDisplay();
     });
 
+
+    ///////////////////////////////////////////////////////////////////
+    // SETUP //////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////
+
+    // Calendar overlay
+    var calOverlay = new Y.Overlay({
+        srcNode: "#calendar-overlay",
+        width: "480px",
+        height: "90%",
+        centered: true});
+
+    // Calendar
+    var cal = new Y.Calendar({
+        contentBox: "#insert-cal-here",
+        width:'100%',
+        showPrevMonth: true,
+        showNextMonth: true,});
+
+
+    ///////////////////////////////////////////////////////////////////
+    // EVENTS /////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////
+
+    // Date change
+    datePivot.on('click', function (e) {
+        // display overlay
+        calOverlay.render();
+        cal.render();
+        calOverlay.show();
+        cal.show();
+
+        // Listen for selection change, update variables, display
+        cal.on("selectionChange", function (ev) {
+            // Will only be one selection unless we allow mutliple
+            pivot = ev.newSelection[0];
+            // Expose main UI
+            calOverlay.hide();
+            cal.hide();
+            refreshDisplay();
+        });
+    });
+
+
+    // Toggle the operation
     dateOp.on('click', function (e) {
-        // toggle plus/minus
+
+        current = this.getHTML();
+        
+        if (current == "+") {
+            op = "-";
+        } else if (current == "-") {
+            op = "+";
+        } else {
+            alert("Well, something is broken.");
+        }
+
+        refreshDisplay();
+
     });
 
 
